25f7cc1e6d34e8527aa184ba6dcb7ffa6cec769a
Manu <thembrown@gmail.com>
2025-03-13 14:31:11 +0100

    basic gcode rendering

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
 import tailwindcss from '@tailwindcss/vite';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { defineConfig, PluginOption } from "vite";
-import checker from "vite-plugin-checker";
-import { viteSingleFile } from "vite-plugin-singlefile";
-
-
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig, PluginOption } from 'vite';
+import checker from 'vite-plugin-checker';
+import { viteSingleFile } from 'vite-plugin-singlefile';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, ...params }) => {
-  let plugins: PluginOption[] = [
+  const plugins: PluginOption[] = [
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react(),
     tailwindcss(),
+    nodePolyfills(),
     checker({
       typescript: true,
       // eslint: {
@@ -26,10 +26,9 @@ export default defineConfig(({ command, ...params }) => {
     plugins,
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "./src"),
+        '@': path.resolve(__dirname, './src'),
       },
     },
     // base: "/present-toolpath",
   };
 });
-
