4a2d9b80f896f18acf9ad853dafd120daa61a423
Manu <thembrown@gmail.com>
2025-06-13 10:47:21 +0200

    backup

--- apps/camNC/eslint.config.mjs
+++ apps/camNC/eslint.config.mjs
@@ -1,9 +1,4 @@
-import { config as baseConfig } from "@wbcnc/eslint-config/react-internal";
+import { config as baseConfig } from '@wbcnc/eslint-config/react-internal';
 
 // /** @type {import("eslint").Linter.FlatConfig[]} */
-export default [
-  ...baseConfig,
-  {
-    ignores: ["public/opencv_js.js"],
-  },
-];
+export default [...baseConfig];
