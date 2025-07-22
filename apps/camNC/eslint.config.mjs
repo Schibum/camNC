70f09f15d93339b2e13058f6e0f6e7e0c30a5bb7
Manu <thembrown@gmail.com>
2025-07-22 14:40:55 +0200

    backup

--- apps/camNC/eslint.config.mjs
+++ apps/camNC/eslint.config.mjs
@@ -1,5 +1,4 @@
-import convexPlugin from '@convex-dev/eslint-plugin';
 import { config as baseConfig } from '@wbcnc/eslint-config/react-internal';
 
 // /** @type {import("eslint").Linter.FlatConfig[]} */
-export default [...baseConfig, ...convexPlugin.configs.recommended];
+export default [...baseConfig];
